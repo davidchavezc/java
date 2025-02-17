4f528526e73557f181f42b87af3d35887f30f69f
davidchavezc <davidchavezc05@gmail.com>
2025-02-16 19:32:46 -0600

    Fix three methods on main
    > Fixed the methods so they actually return the requested values instead
    of just showing them on the console

--- Main.java
+++ Main.java
@@ -2,30 +2,25 @@ public class Main {
   public static void main(String[] args) {
     caracteresEnUltimaPalabra("Anita lava la tina");
   }
-  static void esPalindromo(String word){
+  static boolean esPalindromo(String word){
     String tmpWord = word.toLowerCase().replaceAll("\\s","");
     String reverseWord = "";
     for(int i = 1; tmpWord.length() >= i; i++){
       reverseWord += tmpWord.charAt((tmpWord.length())-i);
     }
-    if(reverseWord.equals(tmpWord)){
-      System.out.println("Sí, el string dado es palíndromo.");
-    }
-    else{
-      System.out.println("No, el string no es palíndromo.");
-    }
+    if(reverseWord.equals(tmpWord)){ return true;} else{return false;}
   }
 
-  static void factorial(int num){
+  static Integer factorial(int num){
     int factorial = 1;
     for(int i = num; i > 0; i--){
       factorial=factorial*i;
     }
-    System.out.println(factorial);
+    return factorial;
   }
 
-  static void caracteresEnUltimaPalabra(String string){
+  static String caracteresEnUltimaPalabra(String string){
     String[] palabrasDelString = string.split("\\s");
-    System.out.println(palabrasDelString[(palabrasDelString.length)-1]);
+    return(palabrasDelString[(palabrasDelString.length)-1]);
   }
 }
